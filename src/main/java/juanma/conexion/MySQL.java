0ea1660292b94be9323fd3dcb1a14ebbc10fdf5e
JuanMa <juan100manuel200@gmail.com>
2025-11-01 03:12:21 -0600

    subiendo datos a base de datos

--- src/main/java/juanma/conexion/MySQL.java
+++ src/main/java/juanma/conexion/MySQL.java
@@ -1,10 +1,10 @@
-
 package juanma.conexion;
 
 import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.PreparedStatement;
+import juanma.parkplus.Vehiculo;
 
 public class MySQL {
     private Connection conectar;
@@ -17,7 +17,7 @@ public class MySQL {
             conectar = DriverManager.getConnection(url, usuario, contraseña);
             return conectar;
         } catch (Exception error) {
-            JOptionPane.showMessageDialog(null,"Error");
+            JOptionPane.showMessageDialog(null,"Error al conectar a la base de datos: " + error.getMessage());
             return null;
         }
     }
@@ -28,7 +28,29 @@ public class MySQL {
                 conectar.close();
             }
         } catch(Exception error) {
-           JOptionPane.showMessageDialog(null,"Error");
+           JOptionPane.showMessageDialog(null,"Error al cerrar la conexión: " + error.getMessage());
+        }
+    }
+    
+    public void subirVehiculo(Vehiculo vehiculo){
+        try {
+            if(conectar != null){
+                String sql = "INSERT INTO vehiculos (Id_Usuario, Tipo, Placa, Marca, Modelo, Color, anio) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement statement = conectar.prepareStatement(sql);
+                statement.setInt(1, vehiculo.Id_Usuario);
+                statement.setString(2, vehiculo.Tipo);
+                statement.setString(3, vehiculo.Placa);
+                statement.setString(4, vehiculo.Marca);
+                statement.setString(5, vehiculo.Modelo);
+                statement.setString(6, vehiculo.Color);
+                statement.setInt(7, vehiculo.Anio);
+
+                statement.executeUpdate(); // Ejecuta el insert
+            } else {
+                JOptionPane.showMessageDialog(null,"No hay conexión a la base de datos");
+            }
+        } catch(Exception error) {
+            JOptionPane.showMessageDialog(null,"Error al insertar vehículo: " + error.getMessage());
         }
     }
 }
