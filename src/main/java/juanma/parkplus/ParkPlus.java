7f6ba46fe32e375132afa06ce8458aab20c3bbfe
JuanMa <juan100manuel200@gmail.com>
2025-11-01 02:02:22 -0600

    conctado a MySQL

--- src/main/java/juanma/parkplus/ParkPlus.java
+++ src/main/java/juanma/parkplus/ParkPlus.java
@@ -1,16 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package juanma.parkplus;
 
-/**
- *
- * @author juan1
- */
+import juanma.conexion.MySQL;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
 public class ParkPlus {
 
     public static void main(String[] args) {
-       
+       MySQL mysql = new MySQL();
+        Connection conectar = mysql.Conexion();
+
+        if(conectar != null){
+            JOptionPane.showMessageDialog(null,"Conectado");
+        } else {
+            JOptionPane.showMessageDialog(null,"Error");
+        }
+
+        mysql.cerrarConexion();
     }
+    
 }
