a06a09bd5515e6b3b4a84301650b2c5f15921e69
JuanMa <juan100manuel200@gmail.com>
2025-11-06 02:54:32 -0600

    Iniciando Interfaz

--- src/main/java/juanma/parkplus/ParkPlus.java
+++ src/main/java/juanma/parkplus/ParkPlus.java
@@ -1,19 +1,12 @@
 package juanma.parkplus;
-
+import Grafica.Login;
 import javax.swing.JOptionPane;
 
 public class ParkPlus {
 
     public static void main(String[] args) {
-        
-        // archivo a leer
-        
-        String rutaCSV1 = "C:\\Users\\juan1\\OneDrive\\Documentos\\JM\\ParkPlus\\vehiculos100.csv";
-       
-        
-// subir datos
-        Funciones.subirDatos(rutaCSV1);
-   
-        JOptionPane.showMessageDialog(null, "carga completa");
-    }
+        Login ventana = new Login();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+ }
 }
