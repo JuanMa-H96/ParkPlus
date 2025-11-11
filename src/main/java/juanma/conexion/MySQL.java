47106e9f55d29f0540566bc214c1d45a5690b67e
JuanMa <juan100manuel200@gmail.com>
2025-11-11 01:03:26 -0600

    Conescion Final

--- src/main/java/juanma/conexion/MySQL.java
+++ src/main/java/juanma/conexion/MySQL.java
@@ -1,56 +1,47 @@
 package juanma.conexion;
 
-import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import juanma.parkplus.Vehiculo;
+import java.sql.SQLException;
 
 public class MySQL {
-    private Connection conectar;
+    private static final String URL="jdbc:mysql://localhost:3306/parkplus";
+    private static final String USER="root";
+    private static final String PASSWORD="";
+    private static Connection conexion=null;
 
-    public Connection Conexion() {
-        try {
-            String url = "jdbc:mysql://localhost:3306/parkplus";
-            String usuario = "root";
-            String contraseña = "";
-            conectar = DriverManager.getConnection(url, usuario, contraseña);
-            return conectar;
-        } catch (Exception error) {
-            JOptionPane.showMessageDialog(null,"Error al conectar a la base de datos: " + error.getMessage());
+    public static Connection Conectar(){
+        try{
+            if(conexion==null || conexion.isClosed()){
+                conexion=DriverManager.getConnection(URL,USER,PASSWORD);
+            }
+            return conexion;
+        }catch(SQLException e){
+            e.printStackTrace();
             return null;
         }
     }
 
-    public void cerrarConexion() {
-        try {
-            if(conectar != null && !conectar.isClosed()){
-                conectar.close();
-            }
-        } catch(Exception error) {
-           JOptionPane.showMessageDialog(null,"Error al cerrar la conexión: " + error.getMessage());
+   
+    
+    public static boolean verificarConexion(){
+        Connection prueba = Conectar();{
+        if(prueba != null){
+            return true;//conecto
+        }else{
+            return false;//no conecto
         }
     }
+    }
     
-    public void subirVehiculo(Vehiculo vehiculo){
-        try {
-            if(conectar != null){
-                String sql = "INSERT INTO vehiculos (Id_Usuario, Tipo, Placa, Marca, Modelo, Color, anio) VALUES (?, ?, ?, ?, ?, ?, ?)";
-                PreparedStatement statement = conectar.prepareStatement(sql);
-                statement.setInt(1, vehiculo.Id_Usuario);
-                statement.setString(2, vehiculo.Tipo);
-                statement.setString(3, vehiculo.Placa);
-                statement.setString(4, vehiculo.Marca);
-                statement.setString(5, vehiculo.Modelo);
-                statement.setString(6, vehiculo.Color);
-                statement.setInt(7, vehiculo.Anio);
-
-                statement.executeUpdate(); // Ejecuta el insert
-            } else {
-                JOptionPane.showMessageDialog(null,"No hay conexión a la base de datos");
+     //cerrar conexion
+    public static void Cerrar(){
+        try{
+            if(conexion!=null && !conexion.isClosed()){
+                conexion.close();
             }
-        } catch(Exception error) {
-            JOptionPane.showMessageDialog(null,"Error al insertar vehículo: " + error.getMessage());
+        }catch(SQLException e){
+            e.printStackTrace();
         }
     }
 }
